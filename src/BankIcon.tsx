b0d728894a685576e7b22b7063082cea8fb374e7
eh. sh <sh.ehsan74.es@gmail.com>
2022-05-07 20:36:02 +0430

    init

---
+++ src/BankIcon.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from 'react';
+// @ts-ignore: Unreachable code error
+import icons from './icons.svg';
+import banksCode from './banksCode';
+
+interface IBank {
+  code: string;
+  name: string;
+  id: string;
+}
+
+interface BankIconProps {
+  digits: number | string;
+  onFindBankName?: (name: string) => void;
+  size?: string;
+  margin?: string;
+}
+
+const BankIcon: FC<BankIconProps> = ({
+  digits,
+  onFindBankName,
+  size = '40px',
+  margin = '8px',
+}) => {
+  const bankId = () => {
+    if (!digits) return <></>;
+
+    if (digits && digits.toString().length === 16) {
+      const code = digits.toString().substr(0, 6);
+      const findBank = (banksCode as IBank[]).find(bank => bank.code === code);
+
+      if (findBank) {
+        if (onFindBankName) onFindBankName(findBank.name);
+        return (
+          <svg
+            className="c-IRBankIcon"
+            style={{ width: size, height: size, margin }}
+          >
+            <use xlinkHref={`${icons}#${findBank.id}`}></use>
+          </svg>
+        );
+      } else {
+        return <></>;
+      }
+    } else {
+      return <></>;
+    }
+  };
+  return bankId();
+};
+
+export default BankIcon;
---
+++ src/banksCode.ts
@@ -0,0 +1,228 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 'Ayandeh',
+    name: 'بانک آینده',
+    code: '636214',
+  },
+  {
+    id: 'EghtesadNovin',
+    name: 'بانک اقتصاد نوین',
+    code: '627412',
+  },
+  {
+    id: 'Ansar',
+    name: 'بانک انصار',
+    code: '627381',
+  },
+  {
+    id: 'IranZamin',
+    name: 'بانک ایران زمین',
+    code: '505785',
+  },
+  {
+    id: 'Parsian',
+    name: 'بانک پارسیان',
+    code: '622106',
+  },
+  {
+    id: 'Parsian',
+    name: 'بانک پارسیان',
+    code: '627884',
+  },
+  {
+    id: 'Pasargad',
+    name: 'بانک پاسارگاد',
+    code: '502229',
+  },
+  {
+    id: 'Pasargad',
+    name: 'بانک پاسارگاد',
+    code: '639347',
+  },
+  {
+    id: 'Postbank',
+    name: 'پست بانک ایران',
+    code: '627760',
+  },
+  {
+    id: 'Tejarat',
+    name: 'بانک تجارت',
+    code: '585983',
+  },
+  {
+    id: 'Tejarat',
+    name: 'بانک تجارت',
+    code: '627353',
+  },
+  {
+    id: 'ToseeTaavon',
+    name: 'بانک توسعه تعاون',
+    code: '502908',
+  },
+  {
+    id: 'ToseeSaderat',
+    name: 'بانک توسعه صادرات',
+    code: '207177',
+  },
+  {
+    id: 'ToseeSaderat',
+    name: 'بانک توسعه صادرات',
+    code: '627648',
+  },
+  {
+    id: 'Hekmat',
+    name: 'بانک حکمت ایرانیان',
+    code: '636949',
+  },
+  {
+    id: 'KhavarMianeh',
+    name: 'بانک خاورمیانه',
+    code: '585949',
+  },
+  {
+    id: 'Dey',
+    name: 'بانک دی',
+    code: '502938',
+  },
+  {
+    id: 'Resalat',
+    name: 'بانک رسالت',
+    code: '504172',
+  },
+  {
+    id: 'Refah',
+    name: 'بانک رفاه کارگران',
+    code: '589463',
+  },
+  {
+    id: 'Saman',
+    name: 'بانک سامان',
+    code: '621986',
+  },
+  {
+    id: 'Sepah',
+    name: 'بانک سپه',
+    code: '589210',
+  },
+  {
+    id: 'Sarmayeh',
+    name: 'بانک سرمایه',
+    code: '639607',
+  },
+  {
+    id: 'Sina',
+    name: 'بانک سینا',
+    code: '639346',
+  },
+  {
+    id: 'Shahr',
+    name: 'بانک شهر',
+    code: '502806',
+  },
+  {
+    id: 'Shahr',
+    name: 'بانک شهر',
+    code: '504706',
+  },
+  {
+    id: 'Saderat',
+    name: 'بانک صادرات ایران',
+    code: '603769',
+  },
+  {
+    id: 'Saderat',
+    name: 'بانک صادرات ایران',
+    code: '903769',
+  },
+  {
+    id: 'SanatMadan',
+    name: 'بانک صنعت و معدن',
+    code: '627961',
+  },
+  {
+    id: 'MehrIran',
+    name: 'بانک قرض الحسنه مهر',
+    code: '639370',
+  },
+  {
+    id: 'Ghavamin',
+    name: 'بانک قوامین',
+    code: '639599',
+  },
+  {
+    id: 'Karafarin',
+    name: 'بانک کارآفرین',
+    code: '627488',
+  },
+  {
+    id: 'Keshavarzi',
+    name: 'بانک کشاورزی',
+    code: '603770',
+  },
+  {
+    id: 'Keshavarzi',
+    name: 'بانک کشاورزی',
+    code: '639217',
+  },
+  {
+    id: 'Gardeshgari',
+    name: 'بانک گردشگری',
+    code: '505416',
+  },
+  {
+    id: 'Gardeshgari',
+    name: 'بانک گردشگری',
+    code: '505426',
+  },
+  {
+    id: 'BankMarkazi',
+    name: 'بانک مرکزی ایران',
+    code: '636797',
+  },
+  {
+    id: 'Maskan',
+    name: 'بانک مسکن',
+    code: '628023',
+  },
+  {
+    id: 'Mellat',
+    name: 'بانک ملت',
+    code: '610433',
+  },
+  {
+    id: 'Mellat',
+    name: 'بانک ملت',
+    code: '991975',
+  },
+  {
+    id: 'Melli',
+    name: 'بانک ملی ایران',
+    code: '170019',
+  },
+  {
+    id: 'Melli',
+    name: 'بانک ملی ایران',
+    code: '603799',
+  },
+  {
+    id: 'MehrIran',
+    name: 'بانک مهر ایران',
+    code: '606373',
+  },
+  {
+    id: 'kosar',
+    name: 'موسسه کوثر',
+    code: '505801',
+  },
+  {
+    id: 'Melal',
+    name: 'موسسه اعتباری ملل',
+    code: '606256',
+  },
+  {
+    id: 'Tosee',
+    name: 'موسسه اعتباری توسعه',
+    code: '628157',
+  },
+];
