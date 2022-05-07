a593cac183d9eb220cbb1962431530e8b82ed601
eh. sh <sh.ehsan74.es@gmail.com>
2022-05-07 21:35:57 +0430

    change tsdx config

--- tsdx.config.js
+++ tsdx.config.js
@@ -2,7 +2,13 @@ const svg = require('rollup-plugin-svg');
 
 module.exports = {
   rollup(config) {
-    config.plugins.push(svg());
+    const external = config.external;
+    config.external = id => (id.match(/.svg$/) ? false : external(id));
+    config.plugins.push(
+      svg({
+        base64: true,
+      })
+    );
     return config;
   },
 };
